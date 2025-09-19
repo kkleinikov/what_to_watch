898713caf0dbef4fc7718c612149f9c9660a69d3
Konstantin-Kleinikov <konstantinkleinikov@gmail.com>
2025-09-19 21:19:01 +0300

    1.07 Refactoring.

---
+++ opinions_app/views.py
@@ -0,0 +1,45 @@
+from random import randrange
+
+from flask import abort, flash, redirect, render_template, url_for
+
+from opinions_app import app, db
+from opinions_app.forms import OpinionForm
+from opinions_app.models import Opinion
+
+
+@app.route('/')
+def index_view():
+    quantity = Opinion.query.count()
+    if not quantity:
+        abort(500)
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    return render_template('opinion.html', opinion=opinion)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_opinion_view():
+    form = OpinionForm()
+    if form.validate_on_submit():
+        text = form.text.data
+        if Opinion.query.filter_by(text=text).first() is not None:
+            flash(
+                'Мнение с таким текстом уже существует',
+                category='warning'
+            )
+            return render_template('add_opinion.html', form=form)
+        opinion = Opinion(
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+        )
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for('opinion_view', id=opinion.id))
+    return render_template('add_opinion.html', form=form)
+
+
+@app.route('/opinions/<int:id>')
+def opinion_view(id):
+    opinion = Opinion.query.get_or_404(id)
+    return render_template('opinion.html', opinion=opinion)
---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
